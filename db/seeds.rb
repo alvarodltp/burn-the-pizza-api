329b2eb739497d018d8e28b422e7fb2f179fd31f
Alvaro De La Torre <alvarodltp@gmail.com>
2019-06-24 10:31:45 -0400

    adds user method to create user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  user = User.create(username: "user1", password: "pw", cheater_level: "extreme")
